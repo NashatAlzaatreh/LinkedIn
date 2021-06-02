42db40e3dcdab4fef2fbb604013994fc6314d50f
NashatAlzaatreh <>
2021-06-02 14:49:23 +0000

    doing Js HW

---
+++ app.js
@@ -0,0 +1,18 @@
+alert('Welcome to your professional community')
+
+var specialty = prompt("what is your specialty, specialty school? ")
+alert('Great, you can find a lot of interest here in ' + specialty + ' field')
+
+
+var job = prompt('Are you looking for a job?')
+
+if(job == 'yes'){
+  alert('sign up and find a good jobs')
+}
+else if (job == 'no'){
+  alert('sign up and do not find a good jobs  ')
+}
+else{
+  alert('sign up and find a good jobs')
+}
+
