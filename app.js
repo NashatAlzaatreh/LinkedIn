a62ef7033b78920a70f21db03e84e2511d2456bb
NashatAlzaatreh <>
2021-06-03 15:41:15 +0000

    lab06

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-//alert('Welcome to your professional community')
+alert('Welcome to your professional community')
 
-//var specialty = prompt("what is your specialty, specialty school? ")
-//alert('Great, you can find a lot of interest here in ' + specialty + ' field')
+var specialty = prompt("what is your specialty, specialty school? ")
+alert('Great, you can find a lot of interest here in ' + specialty + ' field')
 
 
-// var job = prompt('Are you looking for a job?')
+var job = prompt('Are you looking for a job?')
 
-//if(job == 'yes'){
-//  alert('sign up and find a good jobs')
-//}
-//else if (job == 'no'){
-//  alert('sign up and do not find a good jobs  ')
-//}
-//else{
-//  alert('sign up and find a good jobs')
-//}
+if(job == 'yes'){
+ alert('sign up and find a good jobs')
+}
+else if (job == 'no'){
+ alert('sign up and do not find a good jobs  ')
+}
+else{
+ alert('sign up and find a good jobs')
+}
 
-//var job = prompt('How many years of experience do you have?')
+var job = prompt('How many years of experience do you have?')
 
-//while(job > 7){
-  //job = prompt('How many years of experience do you have? we support less than 7 years experience.')
- //}
+while(job > 7){
+  job = prompt('How many years of experience do you have? we support less than 7 years experience.')
+ }
 
-  //alert('sign up and find a good jobs')
+  alert('sign up and find a good jobs')
 
 
 
 
   var pic = prompt('How many job interview do you wants to get ?( 1 or 2 )')
 
-while(pic < 1 && pic > 2 )
+while(pic > 2 )
 {
   pic = prompt('How many job interview do you wants to get ?(1 or 2)')
  }
@@ -42,7 +42,7 @@ if (pic == 1 )
   else {  document.write('<h3> Job interview 1 </h3> <img src="https://engineering.stanford.edu/sites/default/files/styles/full_width_banner_tall/public/interview-a1.jpg" alt="Job interview1" width="250" height="250">')
     
      
-    document.write('<h3> Job interview 1 </h3> <img src="https://www.peninsulapersonnel.com.au/wp-content/uploads/2020/09/Best-HR-Interview-1.png" alt="Job interview1" width="250" height="250">')
+    document.write('<h3> Job interview 2 </h3> <img src="https://www.peninsulapersonnel.com.au/wp-content/uploads/2020/09/Best-HR-Interview-1.png" alt="Job interview1" width="250" height="250">')
     
       }
 
